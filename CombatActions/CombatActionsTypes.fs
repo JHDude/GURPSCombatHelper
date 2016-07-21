7729ad79effd15c3fc5ab589e54f299a9af21e26
Jacob Antoni Hassold <jhdude3@gmail.com>
2016-07-20 20:47:04 -0400

    Renamed base library, kludgy implementation of object-throwing in F#

---
+++ CombatActions/CombatActionsTypes.fs
@@ -0,0 +1,10 @@
+﻿namespace CombatActions
+
+module CombatActionsTypes =
+    type Strength = | Strength of int
+    type BasicLift = | BasicLift of int
+    [<Measure>] type lb
+    [<Measure>] type kg
+    let lbPerKg = 2.20462<lb/kg>
+
+
--- CombatActions/Library1.fs
+++
@@ -1,4 +0,0 @@
-﻿namespace CombatActions
-
-type Class1() = 
-    member this.X = "F#"
