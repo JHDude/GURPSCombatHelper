b2e408d81881842fb652d5c44b11d81ae64ed535
Jacob Antoni Hassold <jhdude3@gmail.com>
2016-08-01 18:58:28 -0400

    Beginning work on form to add actors

---
+++ GURPSCombatHelper/frmAddActor.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace GURPSCombatHelper
+{
+    partial class frmAddActor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbPrimaryStats = new System.Windows.Forms.GroupBox();
+            this.lblST = new System.Windows.Forms.Label();
+            this.lblDX = new System.Windows.Forms.Label();
+            this.lblIQ = new System.Windows.Forms.Label();
+            this.lblHT = new System.Windows.Forms.Label();
+            this.nudST = new System.Windows.Forms.NumericUpDown();
+            this.nudDX = new System.Windows.Forms.NumericUpDown();
+            this.nudIQ = new System.Windows.Forms.NumericUpDown();
+            this.nudHT = new System.Windows.Forms.NumericUpDown();
+            this.grbSecondaryStats = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.lblFP = new System.Windows.Forms.Label();
+            this.lblPerception = new System.Windows.Forms.Label();
+            this.lblWill = new System.Windows.Forms.Label();
+            this.lblHP = new System.Windows.Forms.Label();
+            this.txtBasicLift = new System.Windows.Forms.TextBox();
+            this.lblBasicLift = new System.Windows.Forms.Label();
+            this.lblThrustDamage = new System.Windows.Forms.Label();
+            this.txtThrustDamage = new System.Windows.Forms.TextBox();
+            this.lblSwingDamage = new System.Windows.Forms.Label();
+            this.txtSwingDamage = new System.Windows.Forms.TextBox();
+            this.cmdOK = new System.Windows.Forms.Button();
+            this.cmdCancel = new System.Windows.Forms.Button();
+            this.grbPrimaryStats.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudST)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIQ)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHT)).BeginInit();
+            this.grbSecondaryStats.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grbPrimaryStats
+            // 
+            this.grbPrimaryStats.Controls.Add(this.nudHT);
+            this.grbPrimaryStats.Controls.Add(this.nudIQ);
+            this.grbPrimaryStats.Controls.Add(this.nudDX);
+            this.grbPrimaryStats.Controls.Add(this.nudST);
+            this.grbPrimaryStats.Controls.Add(this.lblHT);
+            this.grbPrimaryStats.Controls.Add(this.lblIQ);
+            this.grbPrimaryStats.Controls.Add(this.lblDX);
+            this.grbPrimaryStats.Controls.Add(this.lblST);
+            this.grbPrimaryStats.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbPrimaryStats.Location = new System.Drawing.Point(12, 12);
+            this.grbPrimaryStats.Name = "grbPrimaryStats";
+            this.grbPrimaryStats.Size = new System.Drawing.Size(106, 176);
+            this.grbPrimaryStats.TabIndex = 0;
+            this.grbPrimaryStats.TabStop = false;
+            this.grbPrimaryStats.Text = "Primary Stats";
+            // 
+            // lblST
+            // 
+            this.lblST.AutoSize = true;
+            this.lblST.Location = new System.Drawing.Point(12, 42);
+            this.lblST.Name = "lblST";
+            this.lblST.Size = new System.Drawing.Size(36, 20);
+            this.lblST.TabIndex = 0;
+            this.lblST.Text = "ST:";
+            // 
+            // lblDX
+            // 
+            this.lblDX.AutoSize = true;
+            this.lblDX.Location = new System.Drawing.Point(9, 74);
+            this.lblDX.Name = "lblDX";
+            this.lblDX.Size = new System.Drawing.Size(39, 20);
+            this.lblDX.TabIndex = 1;
+            this.lblDX.Text = "DX:";
+            // 
+            // lblIQ
+            // 
+            this.lblIQ.AutoSize = true;
+            this.lblIQ.Location = new System.Drawing.Point(15, 106);
+            this.lblIQ.Name = "lblIQ";
+            this.lblIQ.Size = new System.Drawing.Size(33, 20);
+            this.lblIQ.TabIndex = 2;
+            this.lblIQ.Text = "IQ:";
+            // 
+            // lblHT
+            // 
+            this.lblHT.AutoSize = true;
+            this.lblHT.Location = new System.Drawing.Point(11, 138);
+            this.lblHT.Name = "lblHT";
+            this.lblHT.Size = new System.Drawing.Size(37, 20);
+            this.lblHT.TabIndex = 3;
+            this.lblHT.Text = "HT:";
+            // 
+            // nudST
+            // 
+            this.nudST.Location = new System.Drawing.Point(54, 40);
+            this.nudST.Name = "nudST";
+            this.nudST.Size = new System.Drawing.Size(38, 26);
+            this.nudST.TabIndex = 4;
+            // 
+            // nudDX
+            // 
+            this.nudDX.Location = new System.Drawing.Point(54, 72);
+            this.nudDX.Name = "nudDX";
+            this.nudDX.Size = new System.Drawing.Size(38, 26);
+            this.nudDX.TabIndex = 5;
+            // 
+            // nudIQ
+            // 
+            this.nudIQ.Location = new System.Drawing.Point(54, 104);
+            this.nudIQ.Name = "nudIQ";
+            this.nudIQ.Size = new System.Drawing.Size(38, 26);
+            this.nudIQ.TabIndex = 6;
+            // 
+            // nudHT
+            // 
+            this.nudHT.Location = new System.Drawing.Point(54, 136);
+            this.nudHT.Name = "nudHT";
+            this.nudHT.Size = new System.Drawing.Size(38, 26);
+            this.nudHT.TabIndex = 7;
+            // 
+            // grbSecondaryStats
+            // 
+            this.grbSecondaryStats.Controls.Add(this.numericUpDown1);
+            this.grbSecondaryStats.Controls.Add(this.numericUpDown2);
+            this.grbSecondaryStats.Controls.Add(this.numericUpDown3);
+            this.grbSecondaryStats.Controls.Add(this.numericUpDown4);
+            this.grbSecondaryStats.Controls.Add(this.lblFP);
+            this.grbSecondaryStats.Controls.Add(this.lblPerception);
+            this.grbSecondaryStats.Controls.Add(this.lblWill);
+            this.grbSecondaryStats.Controls.Add(this.lblHP);
+            this.grbSecondaryStats.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbSecondaryStats.Location = new System.Drawing.Point(124, 12);
+            this.grbSecondaryStats.Name = "grbSecondaryStats";
+            this.grbSecondaryStats.Size = new System.Drawing.Size(112, 176);
+            this.grbSecondaryStats.TabIndex = 1;
+            this.grbSecondaryStats.TabStop = false;
+            this.grbSecondaryStats.Text = "Secondary Stats";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(54, 141);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(38, 26);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(54, 108);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(38, 26);
+            this.numericUpDown2.TabIndex = 6;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(54, 76);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(38, 26);
+            this.numericUpDown3.TabIndex = 5;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(54, 40);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(38, 26);
+            this.numericUpDown4.TabIndex = 4;
+            // 
+            // lblFP
+            // 
+            this.lblFP.AutoSize = true;
+            this.lblFP.Location = new System.Drawing.Point(12, 143);
+            this.lblFP.Name = "lblFP";
+            this.lblFP.Size = new System.Drawing.Size(36, 20);
+            this.lblFP.TabIndex = 3;
+            this.lblFP.Text = "FP:";
+            // 
+            // lblPerception
+            // 
+            this.lblPerception.AutoSize = true;
+            this.lblPerception.Location = new System.Drawing.Point(7, 110);
+            this.lblPerception.Name = "lblPerception";
+            this.lblPerception.Size = new System.Drawing.Size(41, 20);
+            this.lblPerception.TabIndex = 2;
+            this.lblPerception.Text = "Per:";
+            // 
+            // lblWill
+            // 
+            this.lblWill.AutoSize = true;
+            this.lblWill.Location = new System.Drawing.Point(6, 78);
+            this.lblWill.Name = "lblWill";
+            this.lblWill.Size = new System.Drawing.Size(42, 20);
+            this.lblWill.TabIndex = 1;
+            this.lblWill.Text = "Will:";
+            // 
+            // lblHP
+            // 
+            this.lblHP.AutoSize = true;
+            this.lblHP.Location = new System.Drawing.Point(10, 42);
+            this.lblHP.Name = "lblHP";
+            this.lblHP.Size = new System.Drawing.Size(38, 20);
+            this.lblHP.TabIndex = 0;
+            this.lblHP.Text = "HP:";
+            // 
+            // txtBasicLift
+            // 
+            this.txtBasicLift.Location = new System.Drawing.Point(347, 26);
+            this.txtBasicLift.Name = "txtBasicLift";
+            this.txtBasicLift.ReadOnly = true;
+            this.txtBasicLift.Size = new System.Drawing.Size(100, 20);
+            this.txtBasicLift.TabIndex = 2;
+            // 
+            // lblBasicLift
+            // 
+            this.lblBasicLift.AutoSize = true;
+            this.lblBasicLift.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBasicLift.Location = new System.Drawing.Point(252, 24);
+            this.lblBasicLift.Name = "lblBasicLift";
+            this.lblBasicLift.Size = new System.Drawing.Size(89, 20);
+            this.lblBasicLift.TabIndex = 3;
+            this.lblBasicLift.Text = "Basic Lift:";
+            // 
+            // lblThrustDamage
+            // 
+            this.lblThrustDamage.AutoSize = true;
+            this.lblThrustDamage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThrustDamage.Location = new System.Drawing.Point(276, 50);
+            this.lblThrustDamage.Name = "lblThrustDamage";
+            this.lblThrustDamage.Size = new System.Drawing.Size(65, 20);
+            this.lblThrustDamage.TabIndex = 5;
+            this.lblThrustDamage.Text = "Thrust:";
+            // 
+            // txtThrustDamage
+            // 
+            this.txtThrustDamage.Location = new System.Drawing.Point(347, 52);
+            this.txtThrustDamage.Name = "txtThrustDamage";
+            this.txtThrustDamage.ReadOnly = true;
+            this.txtThrustDamage.Size = new System.Drawing.Size(100, 20);
+            this.txtThrustDamage.TabIndex = 4;
+            // 
+            // lblSwingDamage
+            // 
+            this.lblSwingDamage.AutoSize = true;
+            this.lblSwingDamage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSwingDamage.Location = new System.Drawing.Point(279, 76);
+            this.lblSwingDamage.Name = "lblSwingDamage";
+            this.lblSwingDamage.Size = new System.Drawing.Size(62, 20);
+            this.lblSwingDamage.TabIndex = 7;
+            this.lblSwingDamage.Text = "Swing:";
+            // 
+            // txtSwingDamage
+            // 
+            this.txtSwingDamage.Location = new System.Drawing.Point(347, 78);
+            this.txtSwingDamage.Name = "txtSwingDamage";
+            this.txtSwingDamage.ReadOnly = true;
+            this.txtSwingDamage.Size = new System.Drawing.Size(100, 20);
+            this.txtSwingDamage.TabIndex = 6;
+            // 
+            // cmdOK
+            // 
+            this.cmdOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdOK.Location = new System.Drawing.Point(347, 310);
+            this.cmdOK.Name = "cmdOK";
+            this.cmdOK.Size = new System.Drawing.Size(92, 51);
+            this.cmdOK.TabIndex = 8;
+            this.cmdOK.Text = "OK";
+            this.cmdOK.UseVisualStyleBackColor = true;
+            // 
+            // cmdCancel
+            // 
+            this.cmdCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdCancel.Location = new System.Drawing.Point(448, 310);
+            this.cmdCancel.Name = "cmdCancel";
+            this.cmdCancel.Size = new System.Drawing.Size(92, 51);
+            this.cmdCancel.TabIndex = 9;
+            this.cmdCancel.Text = "Cancel";
+            this.cmdCancel.UseVisualStyleBackColor = true;
+            // 
+            // frmAddActor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 373);
+            this.Controls.Add(this.cmdCancel);
+            this.Controls.Add(this.cmdOK);
+            this.Controls.Add(this.lblSwingDamage);
+            this.Controls.Add(this.txtSwingDamage);
+            this.Controls.Add(this.lblThrustDamage);
+            this.Controls.Add(this.txtThrustDamage);
+            this.Controls.Add(this.lblBasicLift);
+            this.Controls.Add(this.txtBasicLift);
+            this.Controls.Add(this.grbSecondaryStats);
+            this.Controls.Add(this.grbPrimaryStats);
+            this.Name = "frmAddActor";
+            this.Text = "frmAddActor";
+            this.grbPrimaryStats.ResumeLayout(false);
+            this.grbPrimaryStats.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudST)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIQ)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHT)).EndInit();
+            this.grbSecondaryStats.ResumeLayout(false);
+            this.grbSecondaryStats.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbPrimaryStats;
+        private System.Windows.Forms.NumericUpDown nudHT;
+        private System.Windows.Forms.NumericUpDown nudIQ;
+        private System.Windows.Forms.NumericUpDown nudDX;
+        private System.Windows.Forms.NumericUpDown nudST;
+        private System.Windows.Forms.Label lblHT;
+        private System.Windows.Forms.Label lblIQ;
+        private System.Windows.Forms.Label lblDX;
+        private System.Windows.Forms.Label lblST;
+        private System.Windows.Forms.GroupBox grbSecondaryStats;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label lblFP;
+        private System.Windows.Forms.Label lblPerception;
+        private System.Windows.Forms.Label lblWill;
+        private System.Windows.Forms.Label lblHP;
+        private System.Windows.Forms.TextBox txtBasicLift;
+        private System.Windows.Forms.Label lblBasicLift;
+        private System.Windows.Forms.Label lblThrustDamage;
+        private System.Windows.Forms.TextBox txtThrustDamage;
+        private System.Windows.Forms.Label lblSwingDamage;
+        private System.Windows.Forms.TextBox txtSwingDamage;
+        private System.Windows.Forms.Button cmdOK;
+        private System.Windows.Forms.Button cmdCancel;
+    }
+}
