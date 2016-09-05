8aeaa6582797f3e1b4d637ef3a32b2a65cce75c3
Jacob Antoni Hassold <jhdude3@gmail.com>
2016-09-05 12:17:11 -0400

    Adding form properties

--- GURPSCombatHelper/frmAddArmor.cs
+++ GURPSCombatHelper/frmAddArmor.cs
@@ -4,6 +4,19 @@ namespace GURPSCombatHelper
 {
     public partial class frmAddArmor : Form
     {
+        public string name { get; set; }
+        public int eyesDR { get; set; }
+        public int skullDR { get; set; }
+        public int faceDR { get; set; }
+        public int neckDR { get; set; }
+        public int torsoDR { get; set; }
+        public int groinDR { get; set; }
+        public int armDR { get; set; }
+        public int legDR { get; set; }
+        public int handDR { get; set; }
+        public int footDR { get; set; }
+        public bool isFlexible { get; set; }
+
         public frmAddArmor()
         {
             InitializeComponent();
